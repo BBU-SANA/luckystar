2d3c7fe8d18d994bdd39e2ecd99613032c006078
sopheakreth <rethsopheak@gmail.com>
2016-11-28 07:32:27 +0700

    new project

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:2222/luckystar/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:2222/luckystar/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/wamp/www/luckystar/catalog/');
+define('DIR_SYSTEM', 'C:/wamp/www/luckystar/system/');
+define('DIR_IMAGE', 'C:/wamp/www/luckystar/image/');
+define('DIR_LANGUAGE', 'C:/wamp/www/luckystar/catalog/language/');
+define('DIR_TEMPLATE', 'C:/wamp/www/luckystar/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/wamp/www/luckystar/system/config/');
+define('DIR_CACHE', 'C:/wamp/www/luckystar/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/wamp/www/luckystar/system/storage/download/');
+define('DIR_LOGS', 'C:/wamp/www/luckystar/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/wamp/www/luckystar/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/wamp/www/luckystar/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'luckystar');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
